29e20bb46b5243dcb0678f51d1fcb23440f0205d
Gary Stringer <g.b.stringer@exeter.ac.uk>
2016-11-16 09:31:31 +0000

    Added flask code to main

--- flask/simple/test.py
+++ flask/simple/test.py
@@ -4,5 +4,15 @@ Created on 16 Nov 2016
 @author: gbstring
 '''
 
+from flask import Flask
+
+app = Flask(__name)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, World!</h1>'
+
+
+
 if __name__ == '__main__':
-    pass
+    app.run(debug=True)

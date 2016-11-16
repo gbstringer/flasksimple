940494d2c1158831ac7f4765d277970a6384b43c
Gary Stringer <g.b.stringer@exeter.ac.uk>
2016-11-16 12:48:25 +0000

    Added first form

--- flask/simple/test.py
+++ flask/simple/test.py
@@ -5,14 +5,30 @@ Created on 16 Nov 2016
 '''
 
 from flask import Flask, render_template
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+
+''' configure CSRF secret token - should not be stored in svn or git '''
+app.config['SECRET_KEY'] = 'sdhsagfrefcxghdvc bvcdhsvfegfdfvregfrem'
+
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField("What is your name?", validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):

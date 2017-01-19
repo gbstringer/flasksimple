b9576ae9b0d631f69dca517686c4724a88922db7
Gary Stringer <g.b.stringer@exeter.ac.uk>
2017-01-19 14:56:31 +0000

    Flask Simple: minor updates

--- flask/simple/app/models.py
+++ flask/simple/app/models.py
@@ -9,7 +9,7 @@ class Title(db.Model):
     __tablename__ = 'titles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
-    author = db.Column(db.String(127))
+    authors = db.relationship('Person', backref='title', lazy='dynamic')
     volnum = db.Column(db.Integer)
     
     '''
@@ -22,4 +22,11 @@ class Title(db.Model):
     def __repr__(self):
         return '<Title %r>' % self.name
     
+class Person(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(127))
+    title_id = db.Column(db.Integer, db.ForeignKey('titles.id'))
     
+    def __repr__(self):
+        return '<Person %r>' % self.name
